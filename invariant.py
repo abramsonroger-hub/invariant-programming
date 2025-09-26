42ff906371b3d9a611239446d334751926fd7952
abramsonroger-hub <abramson.roger@gmail.com>
2025-09-25 23:08:37 -0500

    Update invariant.py

--- invariant.py
+++ invariant.py
@@ -1,3 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+     # Copyright 2025 Roger Abramson
+     # Licensed under the Apache License, Version 2.0 (the "License");
+     # you may not use this file except in compliance with the License.
+     # You may obtain a copy of the License at [LICENSE.txt]
+
 ledger = Ledger()
 ledger.account("Recurrence")
 ledger.account("ClosedForm")
